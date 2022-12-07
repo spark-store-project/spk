4e8b1c0cc9e24a2d639016ddff292b1f3a9e3bcc
柚子 <40852301+uiYzzi@users.noreply.github.com>
2022-12-07 12:14:29 +0800

    更新

---
+++ spk.js
@@ -0,0 +1,35 @@
+var GetQueryString = function(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+};
+
+function goSPK(){
+    window.location.href=GetQueryString("spk");
+}
+
+//调用 
+var spk=GetQueryString("spk");
+var newspk=spk;
+if(!spk.indexOf('spk://'))
+{
+    newspk = spk.slice(6);
+}
+var jsonUrl="https://cdn.d.store.deepinos.org.cn/"+newspk+"/app.json";
+var titleElement = document.getElementById("title");
+var iconElement = document.getElementById("icon");
+var moreElement = document.getElementById("more");
+var buttonElement = document.getElementById("button1");
+var iconUrl="https://cdn.d.store.deepinos.org.cn/"+newspk+"/icon.png";
+
+fetch(jsonUrl)
+    .then((response) => response.json())
+    .then((json) => {
+        titleElement.innerText=json.Name;
+        moreElement.innerText=json.More;
+        if(json.icons == undefined)
+        {
+            iconElement.src = iconUrl;
+        }
+    });
